bfebed903d33007ea5b395f9e2fca0b4e9f99968
Michael Daum <michael.daum@epicgames.net>
2020-12-24 12:11:54 -0500

    Changed D11a to Data.Array hoping for a speed up

--- D11.hs
+++ D11.hs
@@ -1,25 +1,25 @@
 module D11 where
 
+import Data.Array
+import Data.List
+
 data SeatState = O | E | F deriving(Show, Eq)
 stateFrom :: Char -> SeatState
 stateFrom '.' = F
 stateFrom 'L' = E
 stateFrom '#' = O
-type SeatMap = [[SeatState]]
 
-dims :: SeatMap -> (Int, Int)
-dims seatMap = (length seatMap, length . head $ seatMap)
-
-mapElem seatMap row col = (seatMap !! row) !! col
+mapElem seatMap row col = seatMap!(row, col)
 
 neighborHood seatMap row col = [  mapElem seatMap r c | x <- [-1..1], y <- [-1..1], (x,y) /= (0,0),
                                   let r = row + y,
                                       let c = col + x,
-                                      r >= 0, r < height,
-                                      c >= 0, c < width ]
-  where (height, width) = dims seatMap
+                                      r > 0, r <= height,
+                                      c > 0, c <= width ]
+  where (height, width) = snd . bounds $ seatMap
 
-countOccupied = sum . map (\x -> if x == O then 1 else 0)
+countOccupied :: (Foldable t, Num a) => t SeatState -> a
+countOccupied = foldl (flip $ (+) . (\x -> if x == O then 1 else 0)) 0
 numOccupied seatMap row col = countOccupied $ neighborHood seatMap row col
 
 newState F _ = F
@@ -28,17 +28,21 @@ newState E n | n == 0 = O
 newState O n | n > 3 = E
              | otherwise = O
 
-stepMap oldMap =[ [ newState os nn | c <- [0..(width - 1)], let os = mapElem oldMap r c, let nn = numOccupied oldMap r c] | r <-[0..(height - 1)]]
-  where (height, width) = dims oldMap
+stepMap oldMap = array ((1,1),(rows,cols))  [ ((r,c),newState os nn) | r <- [1..rows], c <- [1..cols], let os = mapElem oldMap r c, let nn = numOccupied oldMap r c]
+  where (rows, cols) = snd . bounds $ oldMap
 
-loadData :: String -> IO SeatMap
+matrixToArray matrix = array ((1,1),(rows, cols)) $ zip indices (concat matrix)
+  where rows = length matrix
+        cols = length $ head matrix
+        indices = [(r,c) | r <- [1..rows], c <- [1..cols]]
+
 loadData fileName = do
   input <- readFile fileName
-  return $ map (map stateFrom) (lines input)
+  return $ matrixToArray $ map (map stateFrom) (lines input)
 
 d11a = do
   seatMap <- loadData "d11.dat"
   let states = iterate stepMap seatMap
       fixedPoint = snd $ last $ takeWhile (uncurry (/=)) $ zip states (tail states)
-      finalOccupied = sum $ map countOccupied fixedPoint
+      finalOccupied = countOccupied fixedPoint
   print finalOccupied
